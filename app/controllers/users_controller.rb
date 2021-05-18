ef3ebd57be8dababbd66cd80bd4a52f597ec0a49
Varujan Tolbakyan <varuj.tolbakyan@gmail.com>
2021-05-18 20:53:49 +0400

    added UsersController and the `login`, `registration` process was organized

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ApplicationController
+  before_action :authenticate!, only: :show # Exclude this route from authentication
+  before_action :set_user, only: :show
+
+  # GET /users/1
+  def show
+    render json: { user: @user }
+  end
+
+  def login
+    user = User.find_by(email: user_params[:email])
+
+    if user&.password_is?(user_params[:password])
+      render_json_auth_token(user_id: user.id)
+    else
+      render json: { error: 'Invalid email or password' }, status: :unauthorized
+    end
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+
+    if @user.save && @user.password_is?(user_params[:password])
+      render_json_auth_token(user_id: @user.id)
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def render_json_auth_token(payload)
+    auth_token = JsonWebToken.encode(payload)
+    render json: { auth_token: auth_token }, status: :ok
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def user_params
+    params.require(:user).permit(:email, :password, :name)
+  end
+end
