13b2f35732e974e39db2e6de54869eac9714aa5d
Varujan Tolbakyan <varuj.tolbakyan@gmail.com>
2021-05-19 17:08:44 +0400

    added migrarton CreateArticles

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_16_185519) do
+ActiveRecord::Schema.define(version: 2021_05_17_185342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.datetime "published_at"
+    t.integer "category", default: 0
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -25,4 +36,5 @@ ActiveRecord::Schema.define(version: 2021_05_16_185519) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "articles", "users"
 end
